adaed53c59004048704bbb66866d27d14a854af9
allenn159 <allenn159@gmail.com>
2021-09-25 22:47:34 -0700

    Updating Movie Description Page

--- src/components/MovieDetailsPage/MovieDetails.jsx
+++ src/components/MovieDetailsPage/MovieDetails.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api/index";
+import { useHistory } from "react-router-dom";
 
-import { Container, Grid } from "@material-ui/core";
+import { Container, Grid, Paper, Typography } from "@material-ui/core";
 import useStyles from "./styles";
 
-const MovieDetails = ({ setSearchResults, movieID }) => {
-  const [movie, setMovie] = useState({});
+import rt from "../../assets/rt.jpg";
+
+const MovieDetails = ({ movieID }) => {
+  const [movie, setMovie] = useState(null);
   const classes = useStyles();
+  let history = useHistory();
 
   const searchByID = async (id) => {
     const { data } = await api.get("/", {
@@ -21,15 +25,30 @@ const MovieDetails = ({ setSearchResults, movieID }) => {
     searchByID(movieID);
   }, []);
 
-  if (!movieID) return "Loading...";
+  console.log(movie);
+
+  if (!movieID) history.push("/");
+  if (!movie) return "Loading...";
+
   return (
     <Container maxWidth="lg">
       <Grid container>
-        <Grid item lg={6}>
-          Sup
+        <Grid item sm={6}>
+          <img src={movie.Poster.replace("SX300", "SX500")} />
+          <Typography variant="h5">{movie.Title}</Typography>
         </Grid>
-        <Grid item lg={6}>
-          Sup
+        <Grid item sm={6}>
+          <Paper>
+            <Typography variant="h5">{movie.Plot}</Typography>
+            <div>
+              <p>Featuring: {movie.Actors}</p>
+              <p>{movie.Year}</p>
+            </div>
+            {/* <div>
+              <img src={rt} />
+              {movie.Ratings[1].Value}
+            </div> */}
+          </Paper>
         </Grid>
       </Grid>
     </Container>
