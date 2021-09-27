277159e8b8e3b3544f481546246e27073d424ff9
allenn159 <allenn159@gmail.com>
2021-09-26 21:38:45 -0700

    Adding styling to movie details component

--- src/components/MovieDetailsPage/MovieDetails.jsx
+++ src/components/MovieDetailsPage/MovieDetails.jsx
@@ -6,9 +6,12 @@ import { Container, Grid, Paper, Typography } from "@material-ui/core";
 import useStyles from "./styles";
 
 import rt from "../../assets/rt.jpg";
+import fresh from "../../assets/fresh.svg";
+import splat from "../../assets/splat.svg";
 
 const MovieDetails = ({ movieID }) => {
   const [movie, setMovie] = useState(null);
+
   const classes = useStyles();
   let history = useHistory();
 
@@ -23,31 +26,53 @@ const MovieDetails = ({ movieID }) => {
 
   useEffect(() => {
     searchByID(movieID);
-  }, []);
+  }, [movieID]);
 
   console.log(movie);
 
-  if (!movieID) history.push("/");
+  if (!movieID) window.location.reload(history.push("/"));
+
   if (!movie) return "Loading...";
 
   return (
-    <Container maxWidth="lg">
+    <Container className={classes.container} maxWidth="lg">
       <Grid container>
         <Grid item sm={6}>
-          <img src={movie.Poster.replace("SX300", "SX500")} />
-          <Typography variant="h5">{movie.Title}</Typography>
+          <img
+            className={classes.image}
+            src={movie.Poster.replace("SX300", "SX500")}
+            alt="Movie Poster"
+          />
         </Grid>
         <Grid item sm={6}>
-          <Paper>
-            <Typography variant="h5">{movie.Plot}</Typography>
-            <div>
-              <p>Featuring: {movie.Actors}</p>
-              <p>{movie.Year}</p>
+          <Paper className={classes.paper}>
+            <div className={classes.paperContent}>
+              <Typography className={classes.movieTitle} variant="h5">
+                {movie.Title}
+              </Typography>
+              <Typography className={classes.movieSummary} variant="h6">
+                {movie.Plot}
+              </Typography>
+              <div>
+                <p>
+                  <strong>Featuring: {movie.Actors}</strong>
+                </p>
+                <p>{movie.Year}</p>
+              </div>
+              {movie.Ratings.filter((e) => e.Source === "Rotten Tomatoes").map(
+                (el) => (
+                  <div key={el.Value}>
+                    <img src={rt} alt="Rotten Tomatoe" />
+                    <p>{el.Value}</p>
+                    {el.Value >= "50%" ? (
+                      <img src={fresh} alt="Fresh" />
+                    ) : (
+                      <img src={splat} alt="Splat" />
+                    )}
+                  </div>
+                )
+              )}
             </div>
-            {/* <div>
-              <img src={rt} />
-              {movie.Ratings[1].Value}
-            </div> */}
           </Paper>
         </Grid>
       </Grid>
--- src/components/MovieDetailsPage/styles.js
+++ src/components/MovieDetailsPage/styles.js
@@ -1,5 +1,24 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    
-  }));
+  container: {
+    marginTop: "50px",
+  },
+  image: {
+    borderRadius: "5px",
+  },
+  paper: {
+    backgroundColor: "#E8F6EF",
+  },
+  paperContent: {
+    padding: "25px",
+  },
+  movieTitle: {
+    fontFamily: "Arvo",
+    color: "#22577A",
+    textAlign: "center",
+  },
+  movieSummary: {
+    marginTop: "10px",
+  },
+}));
